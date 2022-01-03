8b98a26bc44f2f7dcf9ff204020330e1f59f7d27
Joss Dixon <josscdixon@gmail.com>
2022-01-03 13:04:34 +0000

    task text and day added

--- taskme/src/components/Tasks.js
+++ taskme/src/components/Tasks.js
@@ -1,8 +1,10 @@
+import Task from './Task'
+
 const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-      <p key={task.id}>{task.text}</p>
+        <Task key={task.id} task={task}/>
       ))}
     </>
   )
